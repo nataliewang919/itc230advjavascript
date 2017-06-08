b76f71fd133842bd210d7f9789a0c04a45f6092c
nataliewang919 <nataliewang919@kakoshaochies-MacBook-Pro.local>
2017-06-07 19:12:04 -0700

    update for Converting to SPA

--- index.js
+++ index.js
@@ -10,13 +10,28 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 app.use(require("body-parser").urlencoded({extended: true}));
 
+app.use(require("body-parser").json());  
+
 app.use('/api', require('cors')());
 
 let handlebars =  require("express-handlebars");
 app.engine(".html", handlebars({extname: '.html', defaultLayout:'main'}));
 app.set("view engine", ".html");
 
-//api routes
+
+app.get('/', (req,res)=>{
+   Movies.find((err, movies)=>{
+       if(err) return next(err);
+       res.render('home', {movies:JSON.stringify(movies)});
+   })   
+})
+
+app.get('/about', (req,res)=>{
+    res.type('text/html');
+    res.render('about');
+});
+
+
 app.get('/api/v1/movies', (req,res,next)=>{
     Movies.find({},(err, movies)=>{              
     
@@ -33,7 +48,7 @@ app.get('/api/v1/movies', (req,res,next)=>{
 });
 
 app.post('/api/v1/movie/:title', (req,res,next)=>{
-Movies.findOne({title: req.params.title},(err, movies)=>{     
+Movies.findOne({title: req.body.title},(err, movies)=>{     
    if(err||!movies)
    {
         return next(err);
@@ -48,8 +63,8 @@ Movies.findOne({title: req.params.title},(err, movies)=>{
 });
 
 
-app.get('/api/v1/delete/:title', (req,res, next)=>{    
-    Movies.remove({title: req.params.title},(err, result)=>{  
+app.get('/api/v1/delete/:id', (req,res, next)=>{    
+    Movies.remove({_id: req.params.id},(err, result)=>{  
         
         if(err)
     {
@@ -78,7 +93,7 @@ app.get('/api/v1/movie/:title', (req,res,next)=>{
     
     });      
 });
-
+/*
 app.get('/api/v1/add/:title/:director?/:reldate?', (req,res,next)=>{
 //console.log(req.params);
 
@@ -89,10 +104,52 @@ Movies.update({title: title}, {title:title, director: req.params.director, relda
         res.json({updated: result.nModified});
     });
 });
+*/
 
-//end api routes        
+app.post('/api/v1/add/', (req,res, next) => {
+    // find & update existing item, or add new 
+    //console.log('/api/v1/add/');
+    //console.log(req.body);
+    if (!req.body._id) { // insert new document        
+        let movie = new Movies({title:req.body.title, director: req.body.director, reldate: req.body.reldate});
+        movie.save((err,newMovie) => {
+            if (err) return next(err);
+            console.log(newMovie)
+            res.json({updated: 0, _id: newMovie._id});
+        });
+    } else { // update existing document
+        Movies.updateOne({ _id: req.body._id},{title:req.body.title, director: req.body.director, reldate: req.body.reldate },(err, result) => {
+            if (err) return next(err);
+            res.json({updated: result.nModified, _id: req.body._id});
+        });
+    }
+});
 
 
+app.get('/api/v1/add/:title/:director/:reldate', (req,res,next)=>{
+
+let title=req.params.title;
+Movies.update({title: title}, {title:title, director: req.params.director, reldate: req.params.reldate }, {upsert: true }, (err, result) => {
+        if (err) return next(err);
+        // nModified = 0 for new item, = 1+ for updated item 
+        res.json({updated: result.nModified});
+    });
+});
+
+      
+app.use(function(req,res) {
+    res.type('text/plain'); 
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started');    
+});
+
+
+
+/*
 app.get('/', (req,res)=>{
      Movies.find({},(err, movies)=>{              
     if (err) return next(err);   
@@ -153,7 +210,7 @@ app.use((req,res)=>{
         
 app.listen(app.get('port'), function() {
     console.log('Express started');    
-});
+});*/
 
 
 /*
